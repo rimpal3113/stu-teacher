aa992e659a6f0ed732b5c9cb9a0d417db5be7719
rimpal <thakorrimpal30@gmail.com>
2025-11-02 13:34:24 +0530

    link add

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://stu-teacher-orpin.vercel.app"],
+     origin: "https://stu-teacher-orpin.vercel.app",
     credentials: true,
   })
 );
--- frontend/stuteacher/src/pages/RegisterPage.jsx
+++ frontend/stuteacher/src/pages/RegisterPage.jsx
@@ -2,14 +2,14 @@
 
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
-import { Button } from "../components/ui/button"
+import { Button } from "../components/ui/Button"
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "../components/ui/card"
+} from "../components/ui/Card"
 import { Input } from "../components/ui/Input"
 import { Label } from "../components/ui/Label"
 import {
