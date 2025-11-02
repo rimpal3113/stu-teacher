65d93f52fe1384a2f3d6fd3721e092ffadac53c2
rimpal <thakorrimpal30@gmail.com>
2025-11-02 13:39:35 +0530

    like issues

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-   app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://stu-teacher-kmq2.vercel.app"],
-    credentials: true,
-  })
-);
-
+   origin: ["http://localhost:5173", "https://stu-teacher-kmq2.vercel.app"],
     credentials: true,
   })
 );
