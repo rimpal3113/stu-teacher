299e69b01cfa2080a23eced9366f22cc17050e8b
rimpal <thakorrimpal30@gmail.com>
2025-11-03 10:57:39 +0530

    one

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { register, login } from '../controllers/authController.js';
 
 const router = express.Router();
-router.use(corsConfig); 
+
 
 router.post('/register', register);
 router.post('/login', login);
